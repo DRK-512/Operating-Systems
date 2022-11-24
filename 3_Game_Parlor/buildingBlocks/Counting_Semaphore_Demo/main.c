b9d9ca4e2493715c08d57ea292eeb4e3a9020207
DRK-512 <kono6886@kettering.edu>
2022-11-24 01:21:47 -0500

    Semaphore Demo now uses counting semaphore

--- 3_Game_Parlor/buildingBlocks/Counting_Semaphore_Demo/main.c
+++ 3_Game_Parlor/buildingBlocks/Counting_Semaphore_Demo/main.c
@@ -5,31 +5,25 @@
 #include <semaphore.h>
 
 /*
-  The purpose of this program is show how to run a number of threads with a counting semaphore
-  NOTES: With gcc please run with -lpthread
-  		 This is building block 1/3 for the game parlor project
-  		 What this program does is that it creates 8 threads, and prints that threads message onto the terminal
+	The purpose of this program is show how to run a number of threads with a counting semaphore
+	NOTES:  With gcc please run with -lpthread
+			This is building block 1/3 for the game parlor project
+			What this program does is that it creates 8 threads, and prints that threads message onto the terminal
 */
 
-#define NUM_THREADS	8
-char *messages[NUM_THREADS];
-
-void *PrintHello(void *threadid) {
-
-	long taskid = (long) threadid;
-
-	// add a sleep so there is some time for the process to run 
-	sleep(1);
-
-	printf("Thread %ld: %s\n", taskid, messages[taskid]);
-	pthread_exit(NULL);
-}
+#define numT 8
+#define threadLimit 3
+char *messages[numT];
+void *printMessage(); 
+sem_t sema; 
 
 int main() {
 
-	pthread_t threads[NUM_THREADS];
-	long i; 
-	int pError; 
+	pthread_t threads[numT]; // threads we use for testing
+	long i; // made long because pthread_create(x,x,x,VALUE) is expected to be along
+	int pError; // pthreads return value, and the return states if an error as occured 
+	
+	sem_init(&sema, 0, threadLimit); 
 	
 	messages[0] = "English: Hello World!";
 	messages[1] = "Polish: Witaj Świecie!";
@@ -40,15 +34,26 @@ int main() {
 	messages[6] = "Japanese:「こんにちは世界」(Kon'nichiwa Sekai!)"; 
 	messages[7] = "Latin: Salve Mundus!";
 
-	for(i=0;i<NUM_THREADS;i++) {
+	for(i=0;i<numT;i++) {
 	
 		// if pthread does not return 0, we have problems
-		pError = pthread_create(&threads[i], NULL, PrintHello, (void *) i);
+		pError = pthread_create(&threads[i], NULL, printMessage, (void *) i);
 		if (pError) {
 			printf("ERROR; return code from pthread_create() is %d\n", pError);
 			exit(-1);
 		}
 	}
 
+	pthread_exit(NULL);
+}
+
+void *printMessage(void *threadid) {
+
+	long taskid = (long) threadid;
+	 
+	sem_wait(&sema); 
+	printf("Thread %ld: %s\n", taskid, messages[taskid]);
+	sleep(1); 
+	sem_post(&sema); 
 	pthread_exit(NULL);
 }
