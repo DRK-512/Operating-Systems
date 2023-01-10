cccfc0b1ff4734a80391898f18ca178a63409e16
Darek Konopka <69826267+DRK-512@users.noreply.github.com>
2023-01-10 00:54:37 -0500

    Update main.c
    indents fix

--- Process-Synchronization/Game-Parlor/main.c
+++ Process-Synchronization/Game-Parlor/main.c
@@ -1,15 +1,15 @@
 #include "gameProcessing.h"
 
 /* 
- * The program is intended to simulate a game parlor with the use of threads and semaphores
- * people go up to the gameparlor to borrow a # of dice for thier game
- * each game has a different number of dice, and so we have to keep track of how many 
- * dice the game parlor has at a time so we know if people can play a game
+ * The program is intended to simulate a game parlor with the use of threads  
+ * and semaphores people go up to the game parlor to borrow a number of dice for 
+ * for their game each game has a different number of dice, and so we have to 
+ * keep track of the dice so we know if a game can be played at that time or not
  */
 
 int main() {
 
-    // create threads and initalize the mutex
+	// create threads and initalize the mutex
 	pthread_t game_threads[maxDice];
 	sem_init(&mutex, 0, 1);
 	sem_init(&returnMutex, 0, 1);
@@ -24,9 +24,9 @@ int main() {
 		for (i = 0; i < maxDice; i++) {
 			// add params to struct then call thread with struct		
 			char* id = malloc(sizeof(char)); 
-            *id = i; 
+			*id = i; 
 			// we also pass in the ID of the game so we know which one we are talking about
-		    pthread_create(&game_threads[i], NULL, game, id);
+		    	pthread_create(&game_threads[i], NULL, game, id);
 		}
 		for (i = 0; i < maxDice; i++) {
 			pthread_join(game_threads[i], NULL);
