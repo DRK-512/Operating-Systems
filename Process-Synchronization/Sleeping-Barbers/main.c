6d93fcb5d834f411e9be4ca36838fbc59a45fe95
Darek Konopka <69826267+DRK-512@users.noreply.github.com>
2023-01-11 17:25:21 -0500

    Update main.c
    Format fix

--- Process-Synchronization/Sleeping-Barbers/main.c
+++ Process-Synchronization/Sleeping-Barbers/main.c
@@ -19,30 +19,30 @@ int main() {
 
 	// Create threads for our customers and barber
 	pthread_t barberThread[NUM_BARBERS];
-    pthread_t customerThread[NUM_CUSTOMERS];
+    	pthread_t customerThread[NUM_CUSTOMERS];
 	
 	uint8_t i; 
 	for (i = 0; i < NUM_BARBERS; i++) {
 		uint8_t* ID = malloc(sizeof(uint8_t)); 
 		*ID = i;  
-        pthread_create(&barberThread[i], NULL, barber,  ID);
-    }
-    for (i = 0; i < NUM_CUSTOMERS; i++) {
+		pthread_create(&barberThread[i], NULL, barber,  ID);
+	}
+	for (i = 0; i < NUM_CUSTOMERS; i++) {
 		uint8_t* ID = malloc(sizeof(uint8_t)); 
 		*ID = i; 
-        pthread_create(&customerThread[i], NULL, customer, ID);
-    }
+		pthread_create(&customerThread[i], NULL, customer, ID);
+	}
 
-    for (i = 0; i < NUM_BARBERS; i++) {
-        pthread_join(barberThread[i], NULL);
-    }
-    for (i = 0; i < NUM_CUSTOMERS; i++) {
-        pthread_join(customerThread[i], NULL);
-    }
+	for (i = 0; i < NUM_BARBERS; i++) {
+		pthread_join(barberThread[i], NULL);
+	}
+	for (i = 0; i < NUM_CUSTOMERS; i++) {
+		pthread_join(customerThread[i], NULL);
+	}
 
-    sem_destroy(&chairs_mutex);
-    sem_destroy(&sem_customer);
-    sem_destroy(&sem_barber);
+	sem_destroy(&chairs_mutex);
+	sem_destroy(&sem_customer);
+	sem_destroy(&sem_barber);
 
 	// Sleep
 	printf("The barber is sleeping for %d seconds\n", sleepTime);
