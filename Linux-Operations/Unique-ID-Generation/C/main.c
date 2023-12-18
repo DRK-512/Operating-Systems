f544cd4b1ea240bcfbecc5e9c470774912d817cb
DRK-512 <kono6886@kettering.edu>
2023-12-18 17:33:10 -0500

    format fix in Unique-ID

--- Linux-Operations/Unique-ID-Generation/C/main.c
+++ Linux-Operations/Unique-ID-Generation/C/main.c
@@ -6,92 +6,92 @@
 
 void uuid_form(char* output_uuid, char* input_string)
 {
-    // Now I will put the machine-id into the form of a uuid
-    int i, count=0;
-    for(i=0; i<8; i++){
-        output_uuid[i]=input_string[i]; 
-    }
-    count+=8; 
-    output_uuid[count]='-'; 
-    for(i=count; i<count+4; i++){
-        output_uuid[i+1]=input_string[i]; 
-    }
-    count+=5; 
-    output_uuid[count]='-'; 
-    for(i=count; i<count+4; i++){
-        output_uuid[i+1]=input_string[i-1]; 
-    }
-    count+=5; 
-    output_uuid[count]='-'; 
-    for(i=count; i<count+4; i++){
-        output_uuid[i+1]=input_string[i-2]; 
-    }
-    count+=5; 
-    output_uuid[count]='-';
-    for(i=count; i<count+11; i++){
-        output_uuid[i+1]=input_string[i-3]; 
-    }
+	// Now I will put the machine-id into the form of a uuid
+	int i, count=0;
+	for (i=0; i<8; i++)
+		output_uuid[i]=input_string[i]; 
+		
+	count+=8; 
+	output_uuid[count]='-'; 
+	for (i=count; i<count+4; i++)
+		output_uuid[i+1]=input_string[i]; 
+	
+	count+=5; 
+	output_uuid[count]='-'; 
+	for (i=count; i<count+4; i++)
+		output_uuid[i+1]=input_string[i-1]; 
+	
+	count+=5; 
+	output_uuid[count]='-'; 
+	for (i=count; i<count+4; i++)
+		output_uuid[i+1]=input_string[i-2]; 
+
+	count+=5; 
+	output_uuid[count]='-';
+	for (i=count; i<count+11; i++)
+		output_uuid[i+1]=input_string[i-3]; 
+
 }
 
 // My goal with this is to create a random number seed from the machine-ID
 unsigned int createSeedFromID(char* input_ID)
 {
-    unsigned int seed_gen=0; 
-    int i=0, j=1; 
-    while(1){
-        if (input_ID[i]>47 && input_ID[i]<58) {
-            if (seed_gen==0) {
-                seed_gen=input_ID[i] - '0';
-            } else {
-                seed_gen = seed_gen + ((input_ID[i] - '0') * (pow(10,j))); 
-                j++; 
-                if(j>9)
-                    break; 
-            }
-        }
-        i++; 
-        if(i>31)
-            break; 
-    }
+	unsigned int seed_gen=0; 
+	int i=0, j=1; 
+	while (1) {
+		if (input_ID[i]>47 && input_ID[i]<58) {
+			if (seed_gen==0) {
+				seed_gen=input_ID[i] - '0';
+			} else {
+				seed_gen = seed_gen + ((input_ID[i] - '0') * (pow(10,j))); 
+				j++; 
+			if(j>9)
+				break; 
+			}
+		}
+		i++; 
+		if (i>31)
+			break; 
+	}
 }
 
 
 int main() 
 {
-    FILE *idFile = fopen("/etc/machine-id", "r"); 
-    if(idFile==NULL){
-        printf("The /etc/machine-id file is required for this project"); 
-        return -1; 
-    }
+	FILE *idFile = fopen("/etc/machine-id", "r"); 
+	if (idFile==NULL) {
+		printf("The /etc/machine-id file is required for this project"); 
+		return -1; 
+	}
 
-    // I used calloc because I want all to default to 0 as opposed to NULL
-    char* idToCrypt = (char *)calloc(37, sizeof(char)); 
-  
-    fgets(idToCrypt, 32, idFile);  
-    fclose(idFile);
+	// I used calloc because I want all to default to 0 as opposed to NULL
+	char* idToCrypt = (char *)calloc(37, sizeof(char)); 
 
-    printf("My machine ID: %s\n", idToCrypt);
+	fgets(idToCrypt, 32, idFile);  
+	fclose(idFile);
 
-    srand(createSeedFromID(idToCrypt)); 
-    printf("Randomly Generated Clear-Text ID: ");
-    // Now I use the random seed to generate random numbers and capital/lower-case letters
-    int i=0, randomNum=0; 
-    while(i<IDSize) {
-        randomNum = rand();
-        // I want numbers and letters from ASCII table
-        randomNum = 33 + randomNum % (122 - 48 + 1);
-        if((randomNum>47 && randomNum<58) || 
-            (randomNum>64 && randomNum<91) ||
-            (randomNum>96 && randomNum<123)) {
-             
-            if(i%4==0 && i>1){
-                printf("-");
-            }
-            printf("%c", randomNum); 
-            i++;
-        }
-    }
-    free(idToCrypt); 
-    return 0;
+	printf("My machine ID: %s\n", idToCrypt);
+
+	srand(createSeedFromID(idToCrypt)); 
+	printf("Randomly Generated Clear-Text ID: ");
+	// Now I use the random seed to generate random numbers and capital/lower-case letters
+	int i=0, randomNum=0; 
+	while (i<IDSize) {
+		randomNum = rand();
+		// I want numbers and letters from ASCII table
+		randomNum = 33 + randomNum % (122 - 48 + 1);
+		if ((randomNum>47 && randomNum<58) || 
+		(randomNum>64 && randomNum<91) ||
+		(randomNum>96 && randomNum<123)) {
+
+			if (i%4==0 && i>1) {
+				printf("-");
+			}
+				printf("%c", randomNum); 
+				i++;
+			}
+	}
+	free(idToCrypt); 
+	return 0;
 }
 
