a445dd966f874c6f3bb6e0533f371c8d56b5229d
DRK-512 <kono6886@kettering.edu>
2023-12-18 17:22:19 -0500

    Unique ID Gen

--- Linux-Operations/Unique-ID-Generation/C/main.c
+++ Linux-Operations/Unique-ID-Generation/C/main.c
@@ -4,6 +4,8 @@
 #include <uuid/uuid.h>
 #include <math.h>
 
+#define IDSize 32
+
 void uuid_form(char* output_uuid, char* input_string)
 {
     // Now I will put the machine-id into the form of a uuid
@@ -33,6 +35,7 @@ void uuid_form(char* output_uuid, char* input_string)
     }
 }
 
+// My goal with this is to create a random number seed from the machine-ID
 unsigned int createSeedFromID(char* input_ID)
 {
     unsigned int seed_gen=0; 
@@ -54,6 +57,7 @@ unsigned int createSeedFromID(char* input_ID)
     }
 }
 
+
 int main() 
 {
     FILE *idFile = fopen("/etc/machine-id", "r"); 
@@ -62,38 +66,32 @@ int main()
         return -1; 
     }
     
-    char idToCrypt[32];  
+    char* idToCrypt = (char *)calloc(37, sizeof(char)); 
+  
     fgets(idToCrypt, 32, idFile);  
     fclose(idFile);
 
     printf("My machine ID: %s\n", idToCrypt);
-    
-    char *machine_uuid = (char *)calloc(((sizeof(uuid_t)*2)+5), sizeof(char));
-    uuid_form(machine_uuid, idToCrypt); 
-
-    uuid_t myuuid, tempUuid; 
-    uuid_parse(machine_uuid, tempUuid); 
-    uuid_generate_md5(myuuid, tempUuid, "Darek-Hash", strlen("Darek-Hash"));
-
-    printf("Machines MD5 UUID is: %s\n", myuuid); 
 
     srand(createSeedFromID(idToCrypt)); 
     printf("Randomly Generated Clear-Text ID: ");
     int randomNum=0;
     int i=0;  
-    while(i<30) {
+    while(i<IDSize) {
         randomNum = rand();
         randomNum = 33 + randomNum % (122 - 48 + 1);
         if((randomNum>47 && randomNum<58) || 
             (randomNum>64 && randomNum<91) ||
             (randomNum>96 && randomNum<123)) {
-            printf("%c", randomNum); 
-            i++; 
-            if(i%4==0){
+             
+            if(i%4==0 && i>1){
                 printf("-");
             }
+            printf("%c", randomNum); 
+            i++;
         }
     }
-    free(machine_uuid); 
+    free(idToCrypt); 
     return 0;
 }
+
